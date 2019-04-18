7b666395efaabcb073e0ffb685b52e9f5da614e0
evgen <evgentretsyak@gmail.com>
2019-04-18 09:46:13 +0300

    fixed named + added sample

---
+++ sample/src/main/java/com/rizlee/sample/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.rizlee.sample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.rizlee.animatedicon.AnimatedIcon
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
+
+private const val PLAY = 10
+private const val PAUSE = 11
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        animatedIcon.init(PLAY, PAUSE, object : AnimatedIcon.OnAnimatedIconClickListener {
+            override fun onClickEvent(newState: Int) {
+                when (newState) {
+                    PLAY -> Log.i(TAG, "Play state")
+                    PAUSE -> Log.i(TAG, "Pause state")
+                }
+            }
+        })
+    }
+}
